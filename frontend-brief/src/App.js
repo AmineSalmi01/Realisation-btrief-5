9ef90b9e1454a940db6324227c86569a6b271735
Amine Salmi <92025603+AmineSalmi01@users.noreply.github.com>
2022-12-08 20:22:43 +0100

    .

--- frontend-brief/src/App.js
+++ frontend-brief/src/App.js
@@ -1,12 +1,56 @@
 import React, { useEffect } from 'react'
-import { BrowserRouter, Routes, Router } from 'react-router-dom';
-import { Fis }
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import './App.css'; 
+import { Root } from 'postcss';
 
 const App = () => {
+    const activeMenu = true;
+
   return (
-    <h1 className='underline text-3xl' >App</h1 >
+    <div>
+        <BrowserRouter>
+            <div className='flex relative dark:bg-main-dark-bg'>
+                <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+                    <TooltipComponent content="setting" >
+                        <button type='button' className='text-2xl p-3 hover:drop-shadow-xl
+                             hover:bg-light-gray text-white' 
+                             style={{ background: '#FF7A00', borderRadius: '50%' }}>
+                            <FiSettings />
+                        </button>
+                    </TooltipComponent>
+                </div>
+                {activeMenu ? (
+                  <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                    sideBare
+                  </div>
+                ) : (
+                    <div className='w-0 dark:bg-secondary-dark-bg'>
+                    sideBare 
+                    </div>
+                )}
+                    <div className={
+                        `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+                    }>
+                        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                            NavBar
+                        </div> 
+                    </div>
+
+                    <div>
+                        <Routes>
+                            <Route path='/' element="HomePage"/>
+                            <Route path='/Home' element="HomePage"/>
+                            <Route path='/Promotion' element="Promotion"/>
+                            <Route path='/Apprenants' element="Apprenants"/>
+                            <Route path='/Brief' element="Brief"/>
+                        </Routes>
+                    </div>
+            </div>
+        </BrowserRouter>
+    </div>
   )
 }
 

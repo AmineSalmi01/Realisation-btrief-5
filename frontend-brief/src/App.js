4a42034c2625e564d8741462f38b6ea125165728
Amine Salmi <92025603+AmineSalmi01@users.noreply.github.com>
2022-12-08 17:44:14 +0100

    .

--- frontend-brief/src/App.js
+++ frontend-brief/src/App.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { BrowserRouter, Routes, Router } from 'react-router-dom';
+import { Fis }
+
+import './App.css'; 
 
 const App = () => {
   return (
-    <div>App</div>
+    <h1 className='underline text-3xl' >App</h1 >
   )
 }
 

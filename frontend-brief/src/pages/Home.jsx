e5617e451da9aa971ca6c46fe7f4064a896f797f
Amine Salmi <92025603+AmineSalmi01@users.noreply.github.com>
2022-12-11 15:25:21 +0100

    .

--- frontend-brief/src/pages/Home.jsx
+++ frontend-brief/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import {HiMenuAlt3, HiUserGroup, HiClipboardList} from 'react-icons/hi';
 import {AiFillHome} from 'react-icons/ai';
 import {FaSchool} from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 
 
 
@@ -16,8 +16,10 @@ const Home = () => {
   const [open, setOpen] = useState(true);
 
   return (
+    <div>
+    <NavBar />
     <section className='flex gap-6'>
-        <div className={`bg-[#FFFFFF] min-h-screen ${open ? 'w-60': 'w-16'} duration-500 px-4`}>
+        <div className={`bg-[#FFFFFF] min-h-screen ${open ? 'w-60': 'w-16'} duration-500 px-3`}>
             <div className='py-3 flex justify-end'>  
                 <HiMenuAlt3 size={26} className="cursor-pointer hover:text-[#FF7A00] duration-500" onClick={() => setOpen(!open)}/>
             </div>
@@ -27,19 +29,21 @@ const Home = () => {
             <div className='mt-4 flex flex-col gap-4 relative '>
               {menus.map((menu,i)=>(
 
-                <div className='flex items-center text-sm gap-3.5 font-medium p-2 hover:text-[#FF7A00] cursor-pointer'>
+                <div className='flex items-center text-sm gap-3.5 font-medium p-5 hover:text-[#FF7A00] cursor-pointer'>
                   <div>{React.createElement(menu.icon, {size: "20"})}</div> 
-                  <h2 className={`whitespace-pre duration-500 ${!open && 'opacity-0 translate-x-28 overflow-hidden'}`}>{menu.name}</h2>
+                  <h2 
+                    style={{ transitionDelay: `400ms` }}
+                     className={`whitespace-pre duration-500 ${!open && 'opacity-0 overflow-hidden'}`}>
+                    {menu.name}
+                  </h2>
                 </div>
 
               ))}
 
             </div>
         </div>
-        <div className='m-3 text-xl text-gray-900 font-semibold'>
-            amine salmi
-        </div>
-    </section>
+
+    </section> </div>
   )
 }
 

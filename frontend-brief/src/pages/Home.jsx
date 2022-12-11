0f49f714d951d372f6c58643c644d6bde7ceeaee
Amine Salmi <92025603+AmineSalmi01@users.noreply.github.com>
2022-12-11 02:00:56 +0100

    .

--- frontend-brief/src/pages/Home.jsx
+++ frontend-brief/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {HiMenuAlt3, HiUserGroup, HiClipboardList} from 'react-icons/hi';
 import {AiFillHome} from 'react-icons/ai';
 import {FaSchool} from 'react-icons/fa';
@@ -13,11 +13,13 @@ const Home = () => {
     {name : "Apprenants", icon: HiUserGroup},
     {name : "Brief", icon: HiClipboardList},
   ]
+  const [open, setOpen] = useState(true);
+
   return (
     <section className='flex gap-6'>
-        <div className='bg-[#FFFFFF] min-h-screen w-60 px-4'>
+        <div className={`bg-[#FFFFFF] min-h-screen ${open ? 'w-60': 'w-16'} duration-500 px-4`}>
             <div className='py-3 flex justify-end'>  
-                <HiMenuAlt3 size={26} className="cursor-pointer hover:text-[#FF7A00]" />
+                <HiMenuAlt3 size={26} className="cursor-pointer hover:text-[#FF7A00] duration-500" onClick={() => setOpen(!open)}/>
             </div>
             <div>
               
@@ -27,7 +29,7 @@ const Home = () => {
 
                 <div className='flex items-center text-sm gap-3.5 font-medium p-2 hover:text-[#FF7A00] cursor-pointer'>
                   <div>{React.createElement(menu.icon, {size: "20"})}</div> 
-                  <h2>{menu.name}</h2>
+                  <h2 className={`whitespace-pre duration-500 ${!open && 'opacity-0 translate-x-28 overflow-hidden'}`}>{menu.name}</h2>
                 </div>
 
               ))}

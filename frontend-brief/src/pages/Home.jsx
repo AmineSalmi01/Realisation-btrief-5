f9108d460bcf92f5cfca11df080f5e34c22ac5fa
Amine Salmi <92025603+AmineSalmi01@users.noreply.github.com>
2022-12-12 16:59:05 +0100

    .

--- frontend-brief/src/pages/Home.jsx
+++ frontend-brief/src/pages/Home.jsx
@@ -1,15 +1,39 @@
 import React, { useState } from 'react';
-import NavBar from '../components/NavBar';
-import SideBare from '../components/SideBare';
-
+import { BsChevronDown } from 'react-icons/bs'
+import Promotion from '../data-images/Promotion.svg'
 
 
 const Home = () => {
  
 
   return (
-    <div className='w-full max-w-sm bg-white h-10 border border-black-200 rounded-lg'>
-        home
+    <div className='w-full bg-white border border-black-500 rounded-lg'>
+        <div className='flex'>
+          <h2>Home</h2>
+          <BsChevronDown />
+        </div>
+        <div className='flex'>
+          <img src={Promotion} alt='' />
+          <div>
+            <h6>CYBER GANG</h6>
+            <h6>Promo dev web Solicode 22/23 </h6>
+          <div>
+            <span>
+               16 apprenants
+            </span>
+
+            <span>
+               16 apprenantsINT - Maroc
+            </span>
+
+            <span>
+              25/09/2022 
+              24/08/2023
+            </span>
+
+          </div>
+          </div>
+        </div>
     </div>
   )
 }
